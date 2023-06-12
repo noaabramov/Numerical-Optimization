2d72782881e104ebb7bed816e8f038cfae1f5609
noaabramov <noaabr255@gmail.com>
2023-06-12 19:19:55 +0300

    Update examples.py

--- Numerical Optimization HW 1/tests/examples.py
+++ Numerical Optimization HW 1/tests/examples.py
@@ -1,28 +1,27 @@
 import numpy as np
 
-
 def quadratic_1(x, compute_hessian):
-    Q = np.array([[1.0, 0.0], [0.0, 1.0]])
-    f = x.T.dot(Q).dot(x)
+    Q = np.array([[1, 0], [0, 1]])
+    f = np.transpose(x).dot(Q).dot(x)
     g = 2 * Q.dot(x)
     h = 2 * Q if compute_hessian else None
     return f, g, h
 
 
 def quadratic_2(x, compute_hessian):
-    Q = np.array([[1.0, 0.0], [0.0, 100.0]])
-    f = x.T.dot(Q).dot(x)
+    Q = np.array([[1, 0], [0, 100]])
+    f = np.transpose(x).dot(Q).dot(x)
     g = 2 * Q.dot(x)
     h = 2 * Q if compute_hessian else None
     return f, g, h
 
 
 def quadratic_3(x, compute_hessian):
-    Q1 = np.array([[float(np.sqrt(3) / 2), -0.5], [0.5, float(np.sqrt(3) / 2)]])
-    Q2 = np.array([[100.0, 0.0], [0.0, 1.0]])
-    Q_final = Q1.T.dot(Q2).dot(Q1)
+    Q1 = np.array([[np.sqrt(3) / 2, -0.5], [0.5, np.sqrt(3) / 2]])
+    Q2 = np.array([[100, 0], [0, 1]])
+    Q_final = np.transpose(Q1).dot(Q2).dot(Q1)
 
-    f = x.T.dot(Q_final).dot(x)
+    f = np.transpose(x).dot(Q_final).dot(x)
     g = 2 * Q_final.dot(x)
     h = 2 * Q_final if compute_hessian else None
     return f, g, h
@@ -30,18 +29,18 @@ def quadratic_3(x, compute_hessian):
 
 def rosenbrock(x, compute_hessian):
     f = 100 * ((x[1] - (x[0] ** 2)) ** 2) + ((1 - x[0]) ** 2)
-    g = np.array([float(-400 * x[0] * x[1] + 400 * (x[0] ** 3) + 2 * x[0] - 2),
-                  float(200 * x[1] - 200 * (x[0] ** 2))])
+    g = np.array([-400 * x[0] * x[1] + 400 * (x[0] ** 3) + 2 * x[0] - 2,
+                  200 * x[1] - 200 * (x[0] ** 2)])
     h = None
     if compute_hessian:
-        h = np.array([[float(-400 * x[1] + 1200 * (x[0] ** 2) + 2, -400 * x[0])],
-                      [float(-400 * x[0]),         200.0]])
+        h = np.array([[-400 * x[1] + 1200 * (x[0] ** 2) + 2, -400 * x[0]],
+                      [-400 * x[0],         200]])
     return f, g, h
 
 
 def linear(x, compute_hessian):
-    a = np.array([3.0, 3.0])
-    f = a.T.dot(x)
+    a = np.array([3, 3])
+    f = np.transpose(a).dot(x)
     g = a
     h = None
     if compute_hessian:
